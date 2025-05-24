584d73291d464dabbb398a6d3dfe6ab2c887554d
Mauro Furtado <maurofurtado39@gmail.com>
2025-05-23 22:33:57 -0300

    feat(routing): adicionar rotas iniciais para home e perguntas

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/perguntas', (req, res) => {
+    res.render('perguntas');
+});
+
+
+app.listen(8080, () => {
+    console.log('App rodando');
+});
