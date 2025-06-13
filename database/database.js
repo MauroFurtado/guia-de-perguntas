c866267539f088fc529ec0818b72f15649c773dc
Mauro Furtado <maurofurtado39@gmail.com>
2025-06-13 19:00:31 -0300

    feat(database): add Sequelize connection and Pergunta model definition

---
+++ database/database.js
@@ -0,0 +1,6 @@
+const Sequelize = require("sequelize");
+const connection=new Sequelize('guiaperguntas','root','12345678',{
+    host:'localhost',
+    dialect:'mysql'
+});
+module.exports= connection;
